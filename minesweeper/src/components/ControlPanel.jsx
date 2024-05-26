d8ee3cb69b990c942c593a3fd9e1b05c534efd4d
Filipe <filipemgm17@gmail.com>
2024-05-26 19:00:50 +0100

    start button activated

--- minesweeper/src/components/ControlPanel.jsx
+++ minesweeper/src/components/ControlPanel.jsx
@@ -1,7 +1,22 @@
-import React from "react";
+import React, {useState}  from "react";
 import Timer from "./Timer";
 
 function ControlPanel () {
+    const [level,setLevel] = useState(0);
+
+    const handleLevelChange = (event) => {
+      setLevel(parseInt(event.target.value));
+    };
+
+    const startGame = () => {
+      if(level === 0) {
+        alert('Selecione um nível');
+      } else {
+        document.body.innerHTML='';
+      }
+    };
+
+
     const handleTimer = (t) => {
         if (t === 0); //onGameStart();
       };
@@ -11,14 +26,14 @@ function ControlPanel () {
             <div className="space1">
           <form>
             <label htmlFor="btLevel">Nível:</label>
-            <select id="btLevel">
+            <select id="btLevel" value={level} onChange={handleLevelChange}>
             <option value="0">Selecione nivel</option>
             <option value="1">Básico </option>
             <option value="2">Intermédio</option>
             <option value="3">Avançado </option>
             </select>
           </form>
-          <button> Start!</button>
+          <button onClick={startGame}> Start!</button>
           </div>
           <div id="timer">
           <p>Tempo de Jogo:</p>
